9f3fcd288f5e6ef0199bee52162d093cf87642bf
Ash Reesaul <riddhi.reesaul@student.education.wa.edu.au>
2026-04-22 13:41:33 +0800

    Outlined what to code

---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np #used for creating arrays of x values for smooth graphs
+import matplotlib.pyplot as plt #used for plotting graphs/rectangles
+
+#first ask the user to enter a function as a string (for example x**2)
+function_input = input("Enter a function in terms of x (such as x**2 or x**3 + 2): ")
+
+#convert bounds into numbers
+
+#the number of rectangles - integer
+
+#the choice of method - which is converted into all lowercase for consistency
+
+
+
+#DEFINITION - FUNCTION
+
+
+
+#WIDTH OF THE RECTANGLES
+
+
+
+#SETTING UP THE GRAPH
+
+
+
+#MAIN LOOP
+#CHOOSE X BASED ON THE CHOSEN METHOD
+#CALCULATE THE HEIGHT AND AREA
+#PRINT THE WORKING OUT - !!FOR MARKS
+#DRAW THE RECTANGLE ON THE GRAPH
+
+
+
+#FINAL OUTPUT
+
+
+
+#GRAPH SETTINGS
